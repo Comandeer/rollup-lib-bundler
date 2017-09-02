2fecd3012b5304cf5a0b828da5353c192e89caf1
Tomasz Jakut <vepomoc@gmail.com>
2017-09-03 00:15:31 +0200

    test(generateBanner): create tests

---
+++ tests/generateBanner.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const chai = require( 'chai' );
+const expect = chai.expect;
+const generateBanner = require( '../dist/rollup-lib-bundler' ).generateBanner;
+
+describe( 'generateBanner', () => {
+	it( 'produces correct banner', () => {
+		const metadata = {
+			name: 'test-package',
+			author: 'Comandeer',
+			license: 'MIT',
+			version: '9.0.1',
+		};
+		const expected = `/*! test-package v9.0.1 | (c) ${new Date().getFullYear()} Comandeer | MIT license (see LICENSE) */`;
+		const banner = generateBanner( metadata );
+
+		expect( banner ).to.equal( expected );
+	} );
+} );
