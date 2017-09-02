e26d86b7973c610e36518216ed5ebacdf78864b6
Tomasz Jakut <vepomoc@gmail.com>
2017-09-03 00:15:08 +0200

    feat(generateBanner): introduce generateBanner function
    closes #8

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import generateBanner from './generateBanner.js';
 import packageParser from './packageParser.js';
 import bundler from './bundler';
 
@@ -10,3 +11,4 @@ function rlb() {
 export default rlb;
 export { bundler };
 export { packageParser };
+export { generateBanner };
