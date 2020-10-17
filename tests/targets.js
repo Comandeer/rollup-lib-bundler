9b6b8609616e55371c6ab0a8d868add0dce79567
Comandeer <vepomoc@gmail.com>
2020-10-18 00:25:21 +0200

    test(targets): add test for checking if the version is equal to the one in package.json

--- tests/targets.js
+++ tests/targets.js
@@ -1,9 +1,19 @@
 import { expect } from 'chai';
+import { engines } from '../package.json';
 import * as targets from '../src/targets.js';
 
 describe( 'targets', () => {
-	it( 'exports node version in semver format', () => {
-		expect( targets.node ).to.be.a( 'string' );
-		expect( targets.node ).to.match( /^\d+\.\d+\.\d+$/ );
+	describe( 'node', () => {
+		it( 'exports node version in semver format', () => {
+			expect( targets.node ).to.be.a( 'string' );
+			expect( targets.node ).to.match( /^\d+(\.\d+(\.\d+)?)?$/ );
+		} );
+
+		it( 'is the same as in package.json engines.node field', () => {
+			const versionRegex = /\d+(\.\d+(\.\d+)?)?$/;
+			const expected = engines.node.match( versionRegex )[ 0 ];
+
+			expect( targets.node ).to.equal( expected );
+		} );
 	} );
 } );
