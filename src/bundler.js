711c605439eda214292e31aa4297a0dbc756eb8d
Tomasz Jakut <vepomoc@gmail.com>
2021-07-18 21:15:34 +0200

    feat(bundler): add `OutputController` to… well, control output

--- src/bundler.js
+++ src/bundler.js
@@ -4,13 +4,19 @@ import { terser } from 'rollup-plugin-terser';
 import json from '@rollup/plugin-json';
 import babel from '@rollup/plugin-babel';
 import preset from '@babel/preset-env';
+import consoleControlStrings from 'console-control-strings';
 import generateBanner from './generateBanner.js';
 import { node as nodeTarget } from './targets.js';
+import OutputController from './OutputController.js';
 
 async function bundler( metadata ) {
-	const inputConfig = getRollupInputConfig( metadata );
+	const outputController = new OutputController();
+	const inputConfig = getRollupInputConfig( metadata, outputController );
 	const outputConfigCJS = getRollupOutputConfig( metadata, 'cjs' );
 	const otuputConfigESM = getRollupOutputConfig( metadata, 'esm' );
+
+	outputController.showGauge();
+
 	const bundle = await rollup( inputConfig );
 
 	await Promise.all( [
@@ -18,10 +24,14 @@ async function bundler( metadata ) {
 		bundle.write( otuputConfigESM )
 	] );
 
+	outputController.hideGauge();
+	outputController.addLog( `${ consoleControlStrings.color( [ 'bold', 'green' ] ) }Bundling complete!${ consoleControlStrings.color( 'reset' ) }` );
+	outputController.display();
+
 	return bundle.close();
 }
 
-function getRollupInputConfig( metadata ) {
+function getRollupInputConfig( metadata, outputController ) {
 	const plugins = [
 		convertCJS(),
 
@@ -47,6 +57,10 @@ function getRollupInputConfig( metadata ) {
 
 	return {
 		input: metadata.src,
+		/* istanbul ignore next */
+		onwarn( warning ) {
+			outputController.addLog( warning );
+		},
 		plugins
 	};
 }
