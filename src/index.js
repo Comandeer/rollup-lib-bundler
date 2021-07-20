a09603642234f60797a396dbea72cbecb0f0615d
Tomasz Jakut <vepomoc@gmail.com>
2021-07-20 22:39:04 +0200

    refactor(index): unzalgo main API
    BREAKING CHANGE: Make API always async
    
    Closes #197.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import packageParser from './packageParser.js';
 import bundler from './bundler.js';
 
-function rlb() {
+async function rlb() {
 	const metadata = packageParser( 'package.json' );
 
 	return bundler( metadata );
