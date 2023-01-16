343f2325cdf80ec7f136dc7f75e4be041cba6360
Comandeer <vepomoc@gmail.com>
2023-01-16 21:57:38 +0100

    test(bundler): correctly remove test artifacts

--- tests/__helpers__/removeArtifacts.js
+++ tests/__helpers__/removeArtifacts.js
@@ -1,10 +1,23 @@
-import { resolve as resolvePath } from 'path';
-import { rimrafSync as rimraf } from 'rimraf';
+import { rimraf } from 'rimraf';
 
-function removeArtifacts( fixturesPath ) {
-	const distPath = resolvePath( fixturesPath, '*Package', 'dist' );
+let globby;
 
-	rimraf( distPath );
+async function removeArtifacts( fixturesPath ) {
+	if ( !globby ) {
+		const globbyModule = await import( 'globby' );
+		// eslint-disable-next-line require-atomic-updates
+		globby = globbyModule.globby;
+	}
+
+	const distPaths = await globby( [
+		'**/*Package/dist'
+	], {
+		cwd: fixturesPath,
+		onlyDirectories: true,
+		absolute: true
+	} );
+
+	return rimraf( distPaths );
 }
 
 export default removeArtifacts;
