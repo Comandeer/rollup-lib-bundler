a8d920d08808b89856ee86878aefc708151af47e
Comandeer <vepomoc@gmail.com>
2023-01-28 01:16:12 +0100

    test(bundler): remove incorrect `pending` flag for test

--- tests/bundler.js
+++ tests/bundler.js
@@ -282,7 +282,7 @@ describe( 'bundler', () => {
 	// For now let's skip them as the same cases are coverd by the CLI tests anyway.
 	// The downside of this skip: code coverage will have a great drop. Oh, well…
 	describe.skip( 'bundling TS projects', () => { // eslint-disable-line mocha/no-skipped-tests
-		it.pending( 'bundles correctly a simple TS project', async () => {
+		it( 'bundles correctly a simple TS project', async () => {
 			const srcPath = resolvePath( tsPackageFixture, 'src' );
 			const distPath = resolvePath( tsPackageFixture, 'dist' );
 			const indexPath = resolvePath( srcPath, 'index.ts' );
