36ef011f6d51d033574a0f11e7848d753bf08575
Comandeer <vepomoc@gmail.com>
2023-02-12 19:01:46 +0100

    refactor(tests/generatebanner): rewrite to ava

--- tests/generateBanner.js
+++ tests/generateBanner.js
@@ -1,16 +1,15 @@
+import test from 'ava';
 import generateBanner from '../src/generateBanner.js';
 
-describe( 'generateBanner', () => {
-	it( 'produces correct banner', () => {
-		const metadata = {
-			name: 'test-package',
-			author: 'Comandeer',
-			license: 'MIT',
-			version: '9.0.1'
-		};
-		const expected = `/*! test-package v9.0.1 | (c) ${new Date().getFullYear()} Comandeer | MIT license (see LICENSE) */`;
-		const banner = generateBanner( metadata );
+test( 'generateBanner() produces correct banner', ( t ) => {
+	const metadata = {
+		name: 'test-package',
+		author: 'Comandeer',
+		license: 'MIT',
+		version: '9.0.1'
+	};
+	const expected = `/*! test-package v9.0.1 | (c) ${ new Date().getFullYear() } Comandeer | MIT license (see LICENSE) */`;
+	const banner = generateBanner( metadata );
 
-		expect( banner ).to.equal( expected );
-	} );
+	t.is( banner, expected );
 } );
