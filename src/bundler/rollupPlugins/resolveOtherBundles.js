118d435adc0a6160676c966d494a6732e6f83bdc
Comandeer <vepomoc@gmail.com>
2023-02-18 00:31:11 +0100

    feat(bundler): add support for transforming import declarations

--- src/bundler/rollupPlugins/resolveOtherBundles.js
+++ src/bundler/rollupPlugins/resolveOtherBundles.js
@@ -1,4 +1,8 @@
+import { dirname } from 'node:path';
 import { relative as getRelativePath } from 'node:path';
+import { resolve as resolvePath } from 'node:path';
+import { transformAsync } from '@babel/core';
+import * as t from '@babel/types';
 
 function resolveOtherBundles( projectPath, metadata ) {
 	return {
@@ -26,9 +30,59 @@ function resolveOtherBundles( projectPath, metadata ) {
 				id: distPlaceholderPath,
 				external: true
 			};
+		},
+
+		async renderChunk( code, chunk, { file } ) {
+			const chunkFullPath = resolvePath( projectPath, file );
+			const { code: transformedCode, map } = await transformAsync( code, {
+				plugins: [
+					transformImports( projectPath, metadata, chunkFullPath )
+				]
+			} );
+
+			return {
+				code: transformedCode,
+				map
+			};
+		}
+	};
+}
+
+function transformImports( projectPath, metadata, importerFullPath ) {
+	return {
+		visitor: {
+			ImportDeclaration( path ) {
+				const { node } = path;
+				const { value: importee } = node.source;
+
+				if ( !importee.startsWith( 'rlb:' ) ) {
+					return;
+				}
+
+				const importPath = getCorrectImportPath( importee, 'esm' );
+
+				path.replaceWith(
+					t.importDeclaration(
+						node.specifiers,
+						t.stringLiteral( importPath )
+					)
+				);
+			}
 		}
 	};
 
+	function getCorrectImportPath( importee, importType ) {
+		const srcPathRelativeToProject = importee.slice( 4 );
+		const distPathRelativeToProject = metadata[ srcPathRelativeToProject ][ importType ];
+		const distFullPath = resolvePath( projectPath, distPathRelativeToProject );
+		const chunkDirectoryPath = dirname( importerFullPath );
+		const distPathRelativeToChunk = getRelativePath( chunkDirectoryPath, distFullPath );
+		const importPath = distPathRelativeToChunk.startsWith( '.' ) ?
+			distPathRelativeToChunk :
+			`./${ distPathRelativeToChunk }`;
+
+		return importPath;
+	}
 }
 
 export default resolveOtherBundles;
