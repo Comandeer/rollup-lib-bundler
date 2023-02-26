be1a1a3b33d432d4b2e92c497ce4a140143f3c49
Comandeer <vepomoc@gmail.com>
2023-02-26 01:08:59 +0100

    refactor(targets): replace importing JSON module with parsing the file content

--- src/targets.js
+++ src/targets.js
@@ -1,4 +1,11 @@
-import { engines } from '../package.json';
+import { readFile } from 'node:fs/promises';
+import { resolve as resolvePath } from 'node:path';
+import getDirName from '../src/utils/getDirName.js';
+
+const __dirname = getDirName( import.meta.url );
+const packageJSONFilePath = resolvePath( __dirname, '..', 'package.json' );
+const packageJSONFileContent = await readFile( packageJSONFilePath, 'utf8' );
+const { engines } = JSON.parse( packageJSONFileContent );
 
 const node = engines.node.replace( /[<=>~^]/g, '' );
 
