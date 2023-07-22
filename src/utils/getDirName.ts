ca7d547c2f4bbe5d57833c7aa24859210ecd544a
Tomasz Jakut <vepomoc@gmail.com>
2023-07-22 19:32:36 +0200

    refactor(targets): rewrite targets and `getDirName()` to TS

--- src/utils/getDirName.ts
+++ src/utils/getDirName.ts
@@ -1,7 +1,7 @@
 import { fileURLToPath } from 'node:url';
 import { dirname } from 'pathe';
 
-export default function getDirName( url ) {
+export default function getDirName( url: string | URL ): string {
 	const __dirname = dirname( fileURLToPath( url ) );
 
 	return __dirname;
