3aa5ef46956dd3946346ff1bd14977f0885fd878
Tomasz Jakut <vepomoc@gmail.com>
2023-07-28 22:49:12 +0200

    chore(dist): clean after release [ci skip]

--- bin/rlb.mjs
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env node
-/*! @comandeer/rollup-lib-bundler v0.22.1 | (c) 2023 Comandeer | MIT license (see LICENSE) */
-import i from"../dist/rollup-lib-bundler.mjs";await i();
-//# sourceMappingURL=rlb.mjs.map
