12e703a9974942836d6a5573326e345a30568f58
Comandeer <comandeer@comandeer.pl>
2024-05-01 18:03:58 +0200

    chore(eslint): update config

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,6 @@ import eslintConfig from '@comandeer/eslint-config';
 import formattingConfig from '@comandeer/eslint-config/formatting';
 
 export default [
-	...eslintConfig,
-	...formattingConfig
+	...eslintConfig(),
+	...formattingConfig()
 ];
