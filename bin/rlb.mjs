9c6f4170f0540fd1b6bddd2056fbe07518df2f00
Comandeer <comandeer@comandeer.pl>
2024-05-01 18:38:13 +0200

    0.23.0

---
+++ bin/rlb.mjs
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+/*! @comandeer/rollup-lib-bundler v0.23.0 | (c) 2024 Comandeer | MIT license (see LICENSE) */
+import i from"../dist/rollup-lib-bundler.mjs";await i();
+//# sourceMappingURL=rlb.mjs.map
