debe1fbe513f9f806e0e6511f6dec779ca2f6b28
Comandeer <comandeer@comandeer.pl>
2025-03-09 00:58:45 +0100

    chore(dist): clean after release [ci skip]

--- bin/rlb.mjs
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env node
-/*! @comandeer/rollup-lib-bundler v0.26.0 | (c) 2025 Comandeer | MIT license (see LICENSE) */
-import{rlb as i}from"../dist/rollup-lib-bundler.mjs";await i();
-//# sourceMappingURL=rlb.mjs.map
